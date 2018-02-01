bb8cfff3da621f79eb4649874bbad1a06b610f09
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-01 17:14:34 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	GOOGLE_CLIENT_ID:
+		"753635651981-usf3udp3pfsok35sqded6gsvbdn72aa9.apps.googleusercontent.com",
+	GOOGLE_CLIENT_SECRET: "39KNy07WvMU_EpwdWN7InkAd",
+	CALLBACK_URL: "http://localhost:3000/auth/google/callback"
+};
